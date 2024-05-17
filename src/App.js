6a50c1a53349af6581f5822d982f09e32811274a
ertrzupek@gmail.com <ertrzupek@gmail.com>
2024-05-17 16:10:38 -0400

    table setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
+import React from "react";
 import './App.css';
+import d from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      hello!
-    </div>
-  );
+const Row = (props) => {
+    const itemData = props.data;
+    const handleColor = (e) => {
+        e.preventDefault();
+        console.log(e.target.children[0].value);
+        localStorage.setItem("item1", e.target.children[0].value);
+    }
+    return(
+        <tr>
+            <td><button value = {itemData.id} onClick={props.setDisplay}>{itemData.name}</button></td>
+            <td>{itemData.uses}</td>
+            <td>{itemData.location}</td>
+            <td>{itemData.method}</td>
+            <td>
+                <form onSubmit={handleColor}>
+                    <input type="text" id={itemData.id + "_textinput"} name={itemData.name + " needed"} defaultValue={localStorage.getItem("item1")}/>
+                </form>
+            </td>
+        </tr>
+    )
+}
+
+const Item = (props) => {
+    if(props.disp === -1) {
+        return(<div></div>)
+    } else {
+        const itemData = props.data[props.disp]
+        return(
+            <div>
+                all about {itemData.name}
+                <button value = {-1} onClick={props.setDisplay}>close</button>
+            </div>
+        )
+    }
+}
+
+const App = () => {
+    const [data, setData] = React.useState([]);
+    const [display, setDisplay] = React.useState(["block", -1, "none"]);
+    React.useEffect(() => {
+        setData(d);
+    }, []);
+    const changeDisplay = (e) => {
+        e.preventDefault();
+        const disp = (display[1] === -1) ? e.target.value : -1;
+        setDisplay([display[2], disp, display[0]]);
+    }
+    return (
+        <div className="App">
+            <table style = {{display: display[0]}}>
+                <thead>
+                    <tr>
+                        <th>item</th>
+                        <th>location</th>
+                        <th>method</th>
+                        <th>uses</th>
+                        <th>num needed</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, i) => {return(<Row data = {item} setDisplay = {changeDisplay} disp = {display[0]}/>)})}
+                </tbody>
+            </table>
+            <Item disp = {display[1]} data = {data} setDisplay = {changeDisplay}/>
+        </div>
+    );
+    
 }
 
 export default App;
+
